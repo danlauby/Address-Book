63fa47e82005234a755e182198da1eab8394afcd
danlauby <dmlauby@gmail.com>
2017-01-22 14:36:06 -0800

    Add jquery to recieve user inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
+// Business logic
 function Contact(first, last) {
-  this.firstName: first,
-  this.lastName: last
+  this.firstName = first,
+  this.lastName = last
 }
+
+// User logic
+$(function() {
+  $('form#new-contact').submit(function(e) {
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $('ul#contacts').append('<li><span class="contacts">' + newContact.firstName + '</span></li>');
+
+    $('input#new-first-name').val("");
+    $('input#new-last-name').val("");
+    e.preventDefault();
+  });
+});
