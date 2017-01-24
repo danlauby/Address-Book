19f4a508bbe9010dd6968347e1689a4fd89bc086
danlauby <dmlauby@gmail.com>
2017-01-23 19:22:06 -0800

    Update index.html with address inputs and create Adresses constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 // Business logic
 function Contact(first, last) {
-  this.firstName = first,
-  this.lastName = last
+  this.firstName = first;
+  this.lastName = last;
+  this addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
